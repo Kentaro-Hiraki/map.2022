8661fc23d6839b81f4472b2776216d9129c408ae
ken <ken.hira.2016@gmail.com>
2022-05-02 16:23:47 +0900

    地図表示のアプリ実装

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+
+window.onload = function(){
+    const map = createMap();
+}
+
+function createMap() {
+    return new ol.Map({
+        layers: [
+            new ol.layer.Tile({
+              source: new ol.source.OSM()
+            }),
+            // vectorLayer
+        ],
+        target: 'map',
+        view: new ol.View({
+          projection: 'EPSG:4326',
+          center: [139.7528, 35.685175],
+          zoom: 12
+        })
+    });
+} 
